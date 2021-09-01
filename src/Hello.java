c7f23977be9bec98b633aa083498e9d64c5f9c93
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-09-01 11:48:25 -0600

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Jeremiah");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
