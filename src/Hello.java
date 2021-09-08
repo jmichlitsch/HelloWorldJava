0cd53f4be40fea6005055ae9505860266218f9ef
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-09-08 12:56:59 -0600

    add variables

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,15 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Hello Jeremiah");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+
         System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber + myThirdNumber);
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
